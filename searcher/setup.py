97ff247bb9c19be257b1c2bb47ca1d9e44b50326
zwickvitaly <zwickvitaly@gmail.com>
2024-10-31 22:45:58 +0300

    crontab_fix

--- searcher/setup.py
+++ searcher/setup.py
@@ -37,8 +37,7 @@ async def setup_database():
             INDEX idx_bloom (products) TYPE bloom_filter(0.01) GRANULARITY 64
         ) ENGINE = MergeTree()
         PARTITION BY city
-        ORDER BY date DESC
-        SETTINGS index_granularity = 8192;''')
+        ORDER BY date DESC;''')
 
     logger.info("Tables created successfully.")
     tables = client.query("SHOW TABLES")
