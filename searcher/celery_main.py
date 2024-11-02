44ba3e530e079b579336a99874e4a375305e0711
zwickvitaly <zwickvitaly@gmail.com>
2024-11-02 19:11:45 +0300

    experiments

--- searcher/celery_main.py
+++ searcher/celery_main.py
@@ -23,6 +23,6 @@ celery_app.conf.broker_connection_retry_on_startup = True
 celery_app.conf.beat_schedule = {
     "parse_search": {
         "task": "fire_requests",
-        "schedule": crontab(hour="19", minute="13",),
+        "schedule": crontab(hour="19", minute="15",),
     }
 }
