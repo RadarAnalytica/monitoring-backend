612839bfa3e7ce4f95f372a6e32ebfe5974085e1
zwickvitaly <zwickvitaly@gmail.com>
2024-11-02 11:54:50 +0300

    experiments

--- searcher/setup.py
+++ searcher/setup.py
@@ -35,11 +35,11 @@ async def setup_database():
         ''')
 
     client.command('''CREATE TABLE IF NOT EXISTS request_product (
-            city Int64 CODEC(RLE),
-            date Date CODEC(RLE),
-            query String CODEC(LZ4),
-            product UInt32 CODEC(RLE),
-            place UInt16 Codec(LZ4)
+            city Int64 CODEC(LZ4HC),
+            date Date CODEC(DoubleDelta),
+            query String CODEC(LZ4HC),
+            product UInt32 CODEC(LZ4HC),
+            place UInt16 Codec(LZ4HC)
         ) ENGINE = MergeTree()
         PARTITION BY city
         ORDER BY (product, date);''')
