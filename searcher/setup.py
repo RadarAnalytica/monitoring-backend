0c9e249ffe44aee07a9b5fa6007236a1201f57c0
zwickvitaly <zwickvitaly@gmail.com>
2024-11-04 02:08:39 +0300

    experiments

--- searcher/setup.py
+++ searcher/setup.py
@@ -50,7 +50,7 @@ async def setup_database():
                 query String CODEC(LZ4HC),
                 product UInt32 CODEC(LZ4HC),
                 place UInt16 Codec(LZ4HC),
-                INDEX idx_products_2 (city, query, product) TYPE bloom_filter(0.1) GRANULARITY 1
+                INDEX idx_products_2 (city, date, query, product) TYPE bloom_filter(0.1) GRANULARITY 1
             ) ENGINE = MergeTree()
             PARTITION BY city
             ORDER BY (date, query, product);''')
