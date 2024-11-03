59020a1104911e28ba02e1b86f52dbd79b9d2661
zwickvitaly <zwickvitaly@gmail.com>
2024-11-03 18:49:22 +0300

    experiments

--- searcher/server/routers/queries.py
+++ searcher/server/routers/queries.py
@@ -5,8 +5,7 @@ from starlette.responses import JSONResponse
 from server.auth_token.check_token import check_jwt_token
 from server.auth_token.token_scheme import oauth2_scheme
 from server.funcs.get_keywords_data import get_keywords_payload
-from server.funcs.get_product_query_data import get_product_query_payload
-
+from server.funcs.get_product_query_data import get_product_query_payload, get_product_query_payload_v2
 
 query_router = APIRouter()
 
@@ -24,6 +23,21 @@ async def get_product_queries(
         product_id=product_id, interval=interval, city=city
     )
     return result
+
+@query_router.get("/product_queries_v2")
+async def get_product_queries(
+    product_id: int = Query(),
+    city: int = Query(),
+    interval: int = Query(),
+    token: str = Depends(oauth2_scheme),
+):
+    if not check_jwt_token(token):
+        return JSONResponse(status_code=403, content="Unauthorized")
+    result = await get_product_query_payload_v2(
+        product_id=product_id, interval=interval, city=city
+    )
+    return result
+
 
 
 @query_router.post("/get_keywords")
