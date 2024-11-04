a6f9892e3dac56b923aaf4d548b51676b864f198
zwickvitaly <zwickvitaly@gmail.com>
2024-11-04 21:48:36 +0300

    experiments

--- searcher/setup.py
+++ searcher/setup.py
@@ -34,27 +34,18 @@ async def setup_database():
         ORDER BY name;
         ''')
 
+    client.command('''DROP TABLE IF EXISTS request_product;''')
+
     client.command('''CREATE TABLE IF NOT EXISTS request_product (
                 city Int64 CODEC(LZ4HC),
                 date Date CODEC(LZ4HC),
                 query String CODEC(LZ4HC),
                 product UInt32 CODEC(LZ4HC),
                 place UInt16 Codec(LZ4HC),
-                INDEX idx_products_2 (date, query, product) TYPE bloom_filter(0.025) GRANULARITY 1
             ) ENGINE = MergeTree()
             PARTITION BY city
+            PRIMARY KEY (product, query) 
             ORDER BY (date, query, product);''')
-
-
-    client.command('''CREATE TABLE IF NOT EXISTS request_products (
-                    city Int64 CODEC(LZ4HC),
-                    date Date CODEC(LZ4HC),
-                    query String CODEC(LZ4HC),
-                    products Array(UInt32) CODEC(LZ4HC),
-                    INDEX idx_products (products) TYPE inverted GRANULARITY 1
-                ) ENGINE = MergeTree()
-                PARTITION BY city
-                ORDER BY (date)''')
 
     logger.info("Tables created successfully.")
     tables = client.query("SHOW TABLES")
