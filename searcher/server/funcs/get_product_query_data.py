525159713752e29b5b7fe50e098fd48f411da4dc
zwickvitaly <zwickvitaly@gmail.com>
2024-11-04 21:20:04 +0300

    experiments

--- searcher/server/funcs/get_product_query_data.py
+++ searcher/server/funcs/get_product_query_data.py
@@ -33,17 +33,14 @@ async def get_product_query_payload(product_id, interval, city):
 
 async def get_product_db_data_v2(product_id, interval, city):
     async with get_async_connection() as client:
-        query = f"""SELECT sd.query, sd.quantity, groupArray((sd.date, indexOf(sd.products, {product_id}) AS product_index)) AS date_info
-        FROM (SELECT rp.query, rp.date, r.quantity, rp.products
+        query = f"""SELECT rp.query, r.quantity, arrayOf((rp.date, rp.place)) as date_info
         FROM request_product_2 AS rp
         JOIN (SELECT * FROM request FINAL) AS r ON r.query = rp.query
-        WHERE has(rp.products, {product_id})
+        WHERE rp.product = {product_id}
         AND (rp.city = {city})
         AND (rp.date >= toStartOfDay(now() - INTERVAL {interval} DAY))
-        ORDER BY rp.date, r.quantity DESC
-        ) AS sd
-        GROUP BY sd.query, sd.quantity
-        ORDER BY sd.quantity DESC, sd.query;"""
+        GROUP BY rp.date, r.quantity
+        ORDER BY rp.date, r.quantity DESC;"""
         query_result = await client.query(query)
         return query_result.result_rows
 
