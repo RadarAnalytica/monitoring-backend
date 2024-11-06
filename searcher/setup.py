e9666854c6807ce94b1643f3fad2f6264d03d6f6
zwickvitaly <zwickvitaly@gmail.com>
2024-11-06 10:44:13 +0300

    experiments

--- searcher/setup.py
+++ searcher/setup.py
@@ -33,18 +33,18 @@ async def setup_database():
         ) ENGINE = ReplacingMergeTree(updated)
         ORDER BY name;
         ''')
-
+    client.command('''DROP TABLE IF EXISTS request_product''')
     client.command('''CREATE TABLE IF NOT EXISTS request_product (
                     city Int64 CODEC(LZ4HC),
                     date Date CODEC(LZ4HC),
-                    query String CODEC(LZ4HC),
-                    product UInt32 CODEC(LZ4HC),
+                    query String CODEC(ZSTD(5)),
+                    product UInt32 CODEC(ZSTD(5)),
                     place UInt16 Codec(LZ4HC)
                 ) ENGINE = MergeTree()
-                PRIMARY KEY (city, product, date) 
-                ORDER BY (city, product, date, query);''')
+                PRIMARY KEY (city, date, product) 
+                ORDER BY (city, date, product, query);''')
 
-
+    client.command("""INSERT INTO request_product (city, date, query, product, place) SELECT city, date, query, product, place FROM request_product_2 WHERE date = '2024-11-04';""")
     client.command('''CREATE TABLE IF NOT EXISTS request_product_2 (
                         city Int64 CODEC(LZ4HC),
                         date Date CODEC(ZSTD(5)),
@@ -54,7 +54,6 @@ async def setup_database():
                     ) ENGINE = MergeTree()
                     PRIMARY KEY (city, product, date) 
                     ORDER BY (city, product, date, query);''')
-    client.command("""INSERT INTO request_product_2 (city, date, query, product, place) SELECT city, date, query, product, place FROM request_product WHERE date = '2024-11-04';""")
     logger.info("Tables created successfully.")
     tables = client.query("SHOW TABLES")
     logger.info(tables.result_rows)
