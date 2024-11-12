385285a7f7daa01ee1445cf785df5c155648a042
zwickvitaly <zwickvitaly@gmail.com>
2024-11-12 13:25:28 +0300

    experiments

--- searcher/parser/get_init_data.py
+++ searcher/parser/get_init_data.py
@@ -5,26 +5,28 @@ from clickhouse_db.get_async_connection import get_async_connection
 
 async def get_cities_data():
     async with get_async_connection() as client:
-        query = "SELECT id, dest FROM city WHERE updated = (SELECT max(city) FROM request);"
+        query = f"""SELECT id, dest
+        FROM city
+        WHERE (updated = (SELECT max(updated) FROM city)) ORDER BY id;"""
         q = await client.query(query)
-        cities = [(city[0], city[1]) for city in q.result_rows]
-    return cities
+    return q.result_rows
 
 
 async def get_requests_data():
     async with get_async_connection() as client:
-        query = "SELECT id, query FROM request WHERE updated = (SELECT max(updated) FROM request) LIMIT 1000000;"
+        query = f"""SELECT id, query
+                FROM request
+                WHERE (updated = (SELECT max(updated) FROM request)) 
+                ORDER BY id  LIMIT 1000000;"""
         q = await client.query(query)
-        requests = [(r[0], r[1]) for r in q.result_rows]
-    return requests
+    return q.result_rows
 
 
 async def get_dates_data():
     async with get_async_connection() as client:
         query = "SELECT id, date FROM dates WHERE id = (SELECT max(id) FROM dates);"
         q = await client.query(query)
-        last_date = [(dt[0], dt[1]) if dt else (0, None) for dt in q.result_rows]
-    return last_date
+    return q.result_rows
 
 
 async def write_new_date(date_data: tuple[int, date]):
