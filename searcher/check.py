18ab412cc257ba06165a2440a5a82a0e7fd122e2
zwickvitaly <zwickvitaly@gmail.com>
2024-11-12 14:11:34 +0300

    experiments

--- searcher/check.py
+++ searcher/check.py
@@ -25,17 +25,13 @@ async def check(searched_val, city):
         # json_result = [{"date": str(row[0]), "products": row[1]} for row in res.result_rows]
         # logger.info(res.result_rows)
         start = datetime.now()
-        query = f"""SELECT sd.query, sd.quantity, groupArray((sd.date, sd.place)) AS date_info
-        FROM (SELECT r.query as query, r.quantity as quantity, d.date as date, rp.place as place
+        query = f"""SELECT r.query as query, r.quantity as quantity, d.date as date, rp.place as place
         FROM request_product AS rp
         JOIN (SELECT id, query, quantity FROM request FINAL) AS r ON r.id = rp.query
         JOIN dates as d ON d.id = rp.date 
         JOIN city as c ON c.id = rp.city 
         WHERE (rp.product = {searched_val})
-        ORDER BY rp.date, r.quantity DESC
-        ) AS sd
-        GROUP BY sd.query, sd.quantity
-        ORDER BY sd.quantity DESC, sd.query;"""
+        ORDER BY rp.date, r.quantity DESC;"""
         query_result = await client.query(query)
         result = [
             {
