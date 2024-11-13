923064baf3ddb02513e0dbf296d23942c6e1c9dd
zwickvitaly <zwickvitaly@gmail.com>
2024-11-13 14:44:50 +0300

    new_format

--- searcher/server/funcs/get_product_query_data.py
+++ searcher/server/funcs/get_product_query_data.py
@@ -1,28 +1,34 @@
 from clickhouse_db.get_async_connection import get_async_connection
 
-
-async def get_product_db_data():
+async def get_product_db_data(product_id, city, interval):
     async with get_async_connection() as client:
-        query = f"""SELECT count(*) 
-                        FROM request_product;"""
+        query = f"""SELECT sd.query, sd.quantity, groupArray((sd.date, sd.place, sd.advert, sd.natural_place)) AS date_info
+        FROM (SELECT r.query as query, r.quantity as quantity, d.date as date, rp.place as place, rp.advert as advert, rp.natural_place as natural_place
+        FROM request_product AS rp
+        JOIN (SELECT id, query, quantity FROM request FINAL) AS r ON r.id = rp.query
+        JOIN dates as d ON d.id = rp.date
+        JOIN city as c ON c.id = rp.city
+        WHERE (rp.product = {product_id})
+        AND (c.dest = {city})
+        AND (d.date >= toStartOfDay(now() - INTERVAL {interval} DAY))
+        ORDER BY rp.date, r.quantity DESC
+        ) AS sd
+        GROUP BY sd.query, sd.quantity
+        ORDER BY sd.quantity DESC, sd.query;"""
         query_result = await client.query(query)
-    return query_result.result_rows
-
-
-async def get_product_query_payload(product_id, interval, city):
-    query_result = await get_product_db_data()
-    payload = [
-        {
-            "query": row[0],
-            "quantity": row[1],
-            "dates": {
-                str(j_row[0]): j_row[1]
-                for j_row in row[2]
+        result = [
+            {
+                "query": row[0],
+                "quantity": row[1],
+                "dates": {
+                    str(j_row[0]): {"place": j_row[1], "ad": j_row[2].decode() if j_row[2] != b"z" else None,
+                                    "nat": j_row[3] or None}
+                    for j_row in row[2]
+                }
             }
-        }
-        for row in query_result
-    ]
-    return payload
+            for row in query_result.result_rows
+        ]
+    return result
 
 
 
--- searcher/server/routers/queries.py
+++ searcher/server/routers/queries.py
@@ -6,13 +6,14 @@ from settings import logger
 from server.auth_token.check_token import check_jwt_token
 from server.auth_token.token_scheme import oauth2_scheme
 from server.funcs.get_keywords_data import get_keywords_payload
-from server.funcs.get_product_query_data import get_product_query_payload, get_product_db_data
+from server.funcs.get_product_query_data import get_product_db_data
 
 query_router = APIRouter()
 
 
-@query_router.get("/product_queries")
-async def get_product_queries(
+
+@query_router.get("/product_queries_v2")
+async def get_product_queries_v2(
     product_id: int = Query(),
     city: int = Query(),
     interval: int = Query(),
@@ -21,20 +22,7 @@ async def get_product_queries(
     if not check_jwt_token(token):
         return JSONResponse(status_code=403, content="Unauthorized")
     start = datetime.now()
-    result = await get_product_query_payload(
-        product_id=product_id, interval=interval, city=city
-    )
-    logger.info(f"Время выполнения v1 {(datetime.now() - start).total_seconds()}s")
-    return result
-
-@query_router.get("/product_queries_v2")
-async def get_product_queries_v2(
-    token: str = Depends(oauth2_scheme),
-):
-    if not check_jwt_token(token):
-        return JSONResponse(status_code=403, content="Unauthorized")
-    start = datetime.now()
-    result = await get_product_db_data()
+    result = await get_product_db_data(product_id, city, interval)
     logger.info(f"Время выполнения v2 {(datetime.now() - start).total_seconds()}s")
     return result
 
