d5fccae8c7e36c9228fd56f012f5bc331fdb1aca
zwickvitaly <zwickvitaly@gmail.com>
2024-11-14 11:00:41 +0300

    new_format

--- searcher/server/funcs/get_keywords_data.py
+++ searcher/server/funcs/get_keywords_data.py
@@ -5,8 +5,9 @@ async def get_keywords_db_data(products, city=-1257786):
     async with get_async_connection() as client:
         query = f"""SELECT rp.query, r.quantity
         FROM request_product AS rp
-        JOIN (SELECT * FROM request FINAL) AS r ON r.query = rp.query
-        WHERE rp.product IN {tuple(products)} AND city = {city}
+        JOIN (SELECT * FROM request FINAL) AS r ON r.query = rp.query 
+        JOIN city as c ON c.id = rp.city 
+        WHERE rp.product IN {tuple(products)} AND c.dest = {city}
         ORDER BY r.quantity DESC;"""
         query_result = await client.query(query)
         return query_result.result_rows
