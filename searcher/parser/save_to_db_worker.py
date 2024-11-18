62dae4839096d1a0cf714a3f1bb64c4dcff97ee0
zwickvitaly <zwickvitaly@gmail.com>
2024-11-18 10:56:09 +0300

    leak_test

--- searcher/parser/save_to_db_worker.py
+++ searcher/parser/save_to_db_worker.py
@@ -5,10 +5,10 @@ from clickhouse_connect.driver import AsyncClient, Client
 from settings import logger
 
 
-async def save_to_db(items, table, fields, client: Client):
+async def save_to_db(items, table, fields, client: AsyncClient):
     if items:
         try:
-            client.insert(table, items, column_names=fields)
+            await client.insert(table, items, column_names=fields)
             logger.info(f"Запись в БД + city: {items[0][1]}  date: {items[0][2]}")
 
         except Exception as e:
