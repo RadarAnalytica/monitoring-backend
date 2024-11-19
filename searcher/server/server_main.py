6782588ac041b49acfa57d26a9f07bbe9f73711c
zwickvitaly <zwickvitaly@gmail.com>
2024-11-19 11:18:22 +0300

    perf_scaling

--- searcher/server/server_main.py
+++ searcher/server/server_main.py
@@ -6,6 +6,6 @@ from server.routers.queries import query_router
 app = FastAPI()
 
 
-# app.include_router(csv_router, prefix="/api/csv", tags=["csv"])
+app.include_router(csv_router, prefix="/api/csv", tags=["csv"])
 # app.include_router(city_router, prefix="/api/cities", tags=["cities"])
 app.include_router(query_router, prefix="/api/queries", tags=["queries"])
