d8e9a071b101326fedea8b4611d64130eff7b59a
zwickvitaly <zwickvitaly@gmail.com>
2024-11-21 02:52:55 +0300

    telegram_log_alert

--- searcher/server/funcs/get_best_similar.py
+++ searcher/server/funcs/get_best_similar.py
@@ -3,7 +3,7 @@ from clickhouse_db.get_async_connection import get_async_connection
 
 async def get_best_similar_products(product_id, city=1, amount=25):
     async with get_async_connection() as client:
-        query = f"""SELECT DISTINCT r.query, rp.query
+        query = f"""SELECT DISTINCT rp.query
         FROM request_product AS rp
         JOIN (SELECT id, query, quantity FROM request FINAL) AS r ON r.id = rp.query
         JOIN dates as d ON d.id = rp.date
@@ -13,7 +13,7 @@ async def get_best_similar_products(product_id, city=1, amount=25):
         AND (d.date > toStartOfDay(now() - INTERVAL 7 DAY))
         ORDER BY r.quantity DESC LIMIT 10;"""
         query_result = await client.query(query)
-        keywords = [f'"{kw[0]}"' for kw in query_result.result_rows]
+        keywords = [kw[0] for kw in query_result.result_rows]
         query = f"""SELECT DISTINCT rp.product 
                 FROM request_product AS rp
                 JOIN (SELECT id, query, quantity FROM request FINAL) AS r ON r.id = rp.query
@@ -22,7 +22,7 @@ async def get_best_similar_products(product_id, city=1, amount=25):
                 WHERE (rp.product != {product_id})
                 AND (c.id = {city})
                 AND (d.date > toStartOfDay(now() - INTERVAL 7 DAY))
-                AND (r.query IN ({', '.join(keywords)}))
+                AND (r.query IN ({','.join(keywords)}))
                 ORDER BY r.quantity DESC, rp.place LIMIT {amount};"""
         query_result = await client.query(query)
         result = [p[0] for p in query_result.result_rows]
