b814b817e7f8d66120f881799bc0762505ffe927
zwickvitaly <zwickvitaly@gmail.com>
2024-11-21 03:03:20 +0300

    telegram_log_alert

--- searcher/server/funcs/get_best_similar.py
+++ searcher/server/funcs/get_best_similar.py
@@ -20,15 +20,12 @@ async def get_best_similar_products(product_id, city=1, amount=25):
         keywords = [str(kw[0]) for kw in query_result.result_rows]
         logger.info(f"Ключевые слова: {(datetime.now() - start).total_seconds()}s")
         query = f"""SELECT DISTINCT rp.product 
-                FROM request_product AS rp
-                JOIN (SELECT id, query, quantity FROM request FINAL) AS r ON r.id = rp.query
-                JOIN dates as d ON d.id = rp.date
-                JOIN city as c ON c.id = rp.city
+                FROM request_product
                 WHERE (rp.product != {product_id})
-                AND (c.id = {city})
-                AND (d.date > toStartOfDay(now() - INTERVAL 7 DAY))
-                AND (rp.query IN ({','.join(keywords)}))
-                ORDER BY r.quantity DESC, rp.place LIMIT {amount};"""
+                AND (city = {city})
+                AND (date = (SELECT max(id) FROM dates))
+                AND (query IN ({','.join(keywords)}))
+                ORDER BY place LIMIT {amount};"""
         query_result = await client.query(query)
         result = [p[0] for p in query_result.result_rows]
         logger.info(f"Выполнено за: {(datetime.now() - start).total_seconds()}s")
