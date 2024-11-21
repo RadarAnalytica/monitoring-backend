52fcc34e95f3abbee656eacce4bab3ebc99203cf
zwickvitaly <zwickvitaly@gmail.com>
2024-11-21 03:24:50 +0300

    telegram_log_alert

--- searcher/server/funcs/get_best_similar.py
+++ searcher/server/funcs/get_best_similar.py
@@ -29,6 +29,7 @@ async def get_best_similar_products(product_id, city=1, amount=25):
                 AND (city = {city})
                 AND (date = {last_date})
                 AND (query IN ({','.join(keywords)}))
+                AND (place IN (1,2,3,4,5,6,7,8,9,10) 
                 LIMIT {amount};"""
         query_result = await client.query(query)
         result = [p[0] for p in query_result.result_rows]
