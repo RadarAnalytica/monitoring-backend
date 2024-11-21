69d512dab3a4e204a355dc2f851a680326e599bc
zwickvitaly <zwickvitaly@gmail.com>
2024-11-21 11:34:23 +0300

    perf_scaling

--- searcher/server/funcs/get_best_similar.py
+++ searcher/server/funcs/get_best_similar.py
@@ -26,13 +26,14 @@ async def get_best_similar_products(product_id, city=1, amount=25):
         query_result = await client.query(query)
         last_date = query_result.result_rows[0][0]
         logger.info(f"Дата: {(datetime.now() - start).total_seconds()}s")
-        query = f"""SELECT DISTINCT product 
+        query = f"""SELECT product 
                 FROM request_product
                 WHERE (product != {product_id})
                 AND (city = {city})
                 AND (date = {last_date})
                 AND (query IN ({','.join(keywords)}))
                 ORDER BY city, date, query, place 
+                GROUP BY place 
                 LIMIT {amount};"""
         query_result = await client.query(query)
         result = [p[0] for p in query_result.result_rows]
