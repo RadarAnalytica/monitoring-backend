d296e8724ed2dc457dde9bfb7b92213a135e0271
zwickvitaly <zwickvitaly@gmail.com>
2024-12-12 01:53:13 +0300

    fixes

--- searcher/celery_main.py
+++ searcher/celery_main.py
@@ -22,23 +22,23 @@ celery_app.conf.broker_connection_retry_on_startup = True
 celery_app.conf.beat_schedule = {
     "parse_search_moscow": {
         "task": "fire_requests",
-        "schedule": crontab(hour="15", minute="40",),
+        "schedule": crontab(hour="14", minute="00",),
         "args": (1,)
     },
     "parse_search_krasnodar": {
         "task": "fire_requests",
-        "schedule": crontab(hour="18", minute="30",),
+        "schedule": crontab(hour="9", minute="30",),
         "args": (2,)
     },
-    # "parse_search_ekaterinburg": {
-    #     "task": "fire_requests",
-    #     "schedule": crontab(hour="19", minute="30",),
-    #     "args": (3,)
-    # },
-    # "parse_search_vladivostok": {
-    #     "task": "fire_requests",
-    #     "schedule": crontab(hour="23", minute="00",),
-    #     "args": (4,)
-    # }
+    "parse_search_ekaterinburg": {
+        "task": "fire_requests",
+        "schedule": crontab(hour="5", minute="0",),
+        "args": (3,)
+    },
+    "parse_search_vladivostok": {
+        "task": "fire_requests",
+        "schedule": crontab(hour="0", minute="30",),
+        "args": (4,)
+    }
 
 }
