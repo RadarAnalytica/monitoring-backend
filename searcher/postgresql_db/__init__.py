1934d8849076159b54b1cc76357f341f2b548e51
Zwick Vitaly <zwickvitaly@gmail.com>
2025-01-24 09:54:57 +0400

    reschedule

--- searcher/postgresql_db/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import Base
-from db.models import Request, Product, City, RequestProduct
-
-
-__all__ = ["Base", "Request", "Product", "City", "RequestProduct"]
--- searcher/postgresql_db/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .request import Request
-from .product import Product
-from .city import City
-from .request_product import RequestProduct
-
-__all__ = ["Request", "Product", "City", "RequestProduct"]
