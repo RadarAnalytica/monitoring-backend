6e5d05a45902833cd5d5402f41b672e560845b30
Zwick Vitaly <zwickvitaly@gmail.com>
2025-04-12 21:28:16 +0400

    queries

--- searcher/celery_main.py
+++ searcher/celery_main.py
@@ -22,23 +22,23 @@ celery_app.conf.broker_connection_retry_on_startup = True
 celery_app.conf.beat_schedule = {
     "parse_search_moscow": {
         "task": "fire_requests",
-        "schedule": crontab(hour="18", minute="20",),
+        "schedule": crontab(hour="18", minute="40",),
         "args": (1,)
     },
-    "parse_search_krasnodar": {
-        "task": "fire_requests",
-        "schedule": crontab(hour="23", minute="55",),
-        "args": (2,)
-    },
-    "parse_search_ekaterinburg": {
-        "task": "fire_requests",
-        "schedule": crontab(hour="12", minute="0",),
-        "args": (3,)
-    },
-    "parse_search_vladivostok": {
-        "task": "fire_requests",
-        "schedule": crontab(hour="6", minute="0",),
-        "args": (4,)
-    }
+    # "parse_search_krasnodar": {
+    #     "task": "fire_requests",
+    #     "schedule": crontab(hour="23", minute="55",),
+    #     "args": (2,)
+    # },
+    # "parse_search_ekaterinburg": {
+    #     "task": "fire_requests",
+    #     "schedule": crontab(hour="12", minute="0",),
+    #     "args": (3,)
+    # },
+    # "parse_search_vladivostok": {
+    #     "task": "fire_requests",
+    #     "schedule": crontab(hour="6", minute="0",),
+    #     "args": (4,)
+    # }
 
 }
