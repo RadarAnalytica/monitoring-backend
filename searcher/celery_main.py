a9c85b633b1a2733e8ddb9aed64b851a3db8d2d9
Zwick Vitaly <zwickvitaly@gmail.com>
2025-04-14 22:07:30 +0400

    preset dates

--- searcher/celery_main.py
+++ searcher/celery_main.py
@@ -22,7 +22,7 @@ celery_app.conf.broker_connection_retry_on_startup = True
 celery_app.conf.beat_schedule = {
     "parse_search_moscow": {
         "task": "fire_requests",
-        "schedule": crontab(hour="17", minute="40",),
+        "schedule": crontab(hour="18", minute="10",),
         "args": (1,)
     },
     # "parse_search_krasnodar": {
