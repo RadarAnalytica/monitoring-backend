878872ce9149cbf21aea64cbb8185d25c73ee92e
Zwick Vitaly <zwickvitaly@gmail.com>
2025-04-29 18:11:51 +0400

    optimize request final

--- searcher/server/funcs/get_trends_report.py
+++ searcher/server/funcs/get_trends_report.py
@@ -193,9 +193,9 @@ async def get_report_dataset():
     AND
         rfn.growth >= 30
     AND
-        rfn.growth >= 30
+        rfn60.growth >= 30
     AND
-        rfn.growth >= 30
+        rfn90.growth >= 30
     ORDER BY 
         rfn.fs DESC LIMIT 500"""
     async with get_async_connection() as client:
