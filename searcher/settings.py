a643094561dbbfef8dd3c7364dd32b98cb200e1f
Zwick Vitaly <zwickvitaly@gmail.com>
2025-05-08 23:15:50 +0400

    optimize request final

--- searcher/settings.py
+++ searcher/settings.py
@@ -21,7 +21,7 @@ logger.add(stdout, level="DEBUG" if DEBUG else "INFO")
 
 TIMEZONE = timezone("Europe/Moscow")
 
-SEARCH_URL = "https://search.wb.ru/exactmatch/ru/common/v9/search"
+SEARCH_URL = "https://search.wb.ru/exactmatch/ru/common/v12/search"
 
 POPULAR_REQUESTS_URL = "https://seller.wildberries.ru/popular-search-requests"
 
