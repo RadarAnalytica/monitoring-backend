abdf3a1e42bb3888326cdf0fc8ef6a50c43b0c1f
Zwick Vitaly <zwickvitaly@gmail.com>
2025-05-09 16:00:40 +0400

    optimize request final

--- searcher/transfer_to_temp.py
+++ searcher/transfer_to_temp.py
@@ -86,15 +86,15 @@ FROM
     client.close()
 
 
-dates = list(range(89, 109))
+dates = list(range(89, 107))
 dates.sort(reverse=True)
 print(dates[0], "-", dates[-1])
 
 for i_ in dates:
     left = 0
     right = 450000000
-    if i_ == 108:
-        left = 360000000
+    if i_ == 106:
+        left = 379805631
     transfer(left, right, 10000000, 1, i_)
     time.sleep(5)
 
