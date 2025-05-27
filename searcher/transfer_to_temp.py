1fbbde62b6173775935b88acb2ec3853302d1d3f
Zwick Vitaly <zwickvitaly@gmail.com>
2025-05-27 22:44:01 +0400

    formatting

--- searcher/transfer_to_temp.py
+++ searcher/transfer_to_temp.py
@@ -8,10 +8,10 @@ def transfer(date):
     client = clickhouse_connect.get_client(**CLICKHOUSE_CONFING)
     logger.info(f"DATE {date}")
     client.command(
-            f"""INSERT INTO radar.supplier_aggregates
+            f"""INSERT INTO radar.brand_aggregates
 SELECT
     date,
-    supplier_id,
+    brand_id,
     avgState(place) AS place_avg_state,
     sumState(cpm) AS cpm_sum_state,
     sumState(if(advert = 'b', cpm, 0)) AS cpm_sum_b_state,
@@ -21,12 +21,12 @@ SELECT
     avgState(if(advert = 'c', toNullable(cpm), NULL)) AS cpm_avg_c_state,
     uniqExactState(product) AS unique_products_state
 FROM radar.request_product where city = 1 and date = {date}
-GROUP BY date, supplier_id;"""
+GROUP BY date, brand_id;"""
         )
     client.close()
 
 
-dates = list(range(1, 111))
+dates = list(range(1, 126))
 dates.sort(reverse=True)
 print(dates[0], "-", dates[-1])
 
