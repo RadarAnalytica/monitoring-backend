6618732e23a5f0fe143956ddbeee285d7e00ec4b
Zwick Vitaly <zwickvitaly@gmail.com>
2025-06-05 15:45:07 +0400

    formatting

--- searcher/server/funcs/upload_requests_data.py
+++ searcher/server/funcs/upload_requests_data.py
@@ -30,7 +30,7 @@ async def upload_request_growth_worker(
     await client.insert(
         "request_growth",
         requests_slice,
-        column_names=["query_id", "new_date", "g30", "g60", "g90", "sum_30", "subject_id"],
+        column_names=["query_id", "date", "g30", "g60", "g90", "sum30", "subject_id"],
     )
     logger.info("Start of part DB renewal - request_growth")
 
