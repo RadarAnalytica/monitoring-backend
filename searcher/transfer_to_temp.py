64bcd58e1dd0d1ce0e9971eb75805ea2fc684cea
Zwick Vitaly <zwickvitaly@gmail.com>
2025-06-25 13:50:08 +0400

    formatting

--- searcher/transfer_to_temp.py
+++ searcher/transfer_to_temp.py
@@ -6,44 +6,26 @@ from server.funcs.upload_requests_data import recount_growth_by_date
 from settings import logger
 
 async def main():
-    stmt = """INSERT INTO query_products_daily
-SELECT
-    query,
-    date,
-    groupUniqArrayState(product) AS products
-FROM request_product WHERE city = 1 AND date = {date} AND product BETWEEN 1 AND 200000000
-GROUP BY query, date;
-"""
-    stmt_1 = """INSERT INTO query_products_daily
-SELECT
-    query,
-    date,
-    groupUniqArrayState(product) AS products
-FROM request_product WHERE city = 1 AND date = {date} AND product BETWEEN 200000001 AND 280000000
-GROUP BY query, date"""
-    stmt_2 = """INSERT INTO query_products_daily
-SELECT
-    query,
-    date,
-    groupUniqArrayState(product) AS products
-FROM request_product WHERE city = 1 AND date = {date} AND product BETWEEN 280000001 AND 350000000
-GROUP BY query, date"""
-    stmt_3 = """INSERT INTO query_products_daily
-SELECT
-    query,
-    date,
-    groupUniqArrayState(product) AS products
-FROM request_product WHERE city = 1 AND date = {date} AND product BETWEEN 350000001 AND 500000000
-GROUP BY query, date"""
-    dates = [i for i in range(152)]
-    stmts = [stmt, stmt_1, stmt_2, stmt_3]
+    step = 50000000
+    l = 1
+    r = 500000001
+    dates = [i for i in range(151)]
     dates.reverse()
     async with get_async_connection() as client:
         for d in dates:
             logger.info(f"date: {d}")
-            for s in stmts:
-                st = s.format(date=d)
-                await client.command(st)
+            for i in range(l, r, step):
+                left = i
+                right = i + step - 1
+                stmt = f"""INSERT INTO query_products_daily
+SELECT
+    query,
+    date,
+    groupUniqArrayState(product) AS products
+FROM request_product WHERE city = 1 AND date = {d} AND product BETWEEN {left} AND {right}
+GROUP BY query, date;
+"""
+                await client.command(stmt)
 
 if __name__ == '__main__':
     asyncio.run(main())
