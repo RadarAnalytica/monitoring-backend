5fd2d08ccc160e7c53333871343e3d8f7e38a6b1
Zwick Vitaly <zwickvitaly@gmail.com>
2025-06-26 04:00:44 +0400

    formatting

--- searcher/transfer_to_temp.py
+++ searcher/transfer_to_temp.py
@@ -6,7 +6,7 @@ from server.funcs.upload_requests_data import recount_growth_by_date
 from settings import logger
 
 async def main():
-    step = 50000000
+    step = 25000000
     l = 1
     r = 500000001
     dates = [i for i in range(156)]
