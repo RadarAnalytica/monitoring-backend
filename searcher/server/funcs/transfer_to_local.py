9736df447813695f82d98aa9f0f79bb920fdceb8
Zwick Vitaly <zwickvitaly@gmail.com>
2025-07-09 11:24:17 +0400

    formatting

--- searcher/server/funcs/transfer_to_local.py
+++ searcher/server/funcs/transfer_to_local.py
@@ -1,6 +1,8 @@
 from clickhouse_db.get_async_connection import get_async_connection
 
 async def transfer_aggregates_to_local():
+    stmt_clear = """TRUNCATE TABLE wb_id_extended_local"""
     stmt = """INSERT INTO wb_id_extended_local SELECT * FROM wb_id_extended"""
     async with get_async_connection(send_receive_timeout=3600) as client:
+        await client.command(stmt_clear)
         await client.command(stmt)
